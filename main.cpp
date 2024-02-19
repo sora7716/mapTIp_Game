dfd26d9af0e3c509fd4dd1e752b6a512fcf89f8e
konnitiwa4242@gmail.com <konnitiwa4242@gmail.com>
2024-02-19 23:59:45 +0900

    ウィンドウのタイトルバーの変更

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Novice.h>
 
-const char kWindowTitle[] = "学籍番号";
+const char kWindowTitle[] = "GC1A_01_イイヅカ_ソラ_title";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
